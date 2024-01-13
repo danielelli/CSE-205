0a4ae632906a412f7df0d65433c1c373fd2c5abb
Daniel <90986300+danielelli@users.noreply.github.com>
2024-01-12 18:10:25 -0600

    Update balloonInflation.java

--- fundamental-data-types/balloonInflation.java
+++ fundamental-data-types/balloonInflation.java
@@ -1,24 +1,28 @@
 import java.util.Scanner;
 
-public class Balloon
+public class Balloon 
 {
-  public static void main(String[] args)
-  {
-    Scanner in = new Scanner(System.in);
-    System.out.print("Diameter: ");
-    double diameter = in.nextDouble();
-
-    double volume1 = (3.14 * (diameter * diameter * diameter)) / 6;
-    double diameter++;
-    double volume2 = (3.14 * (diameter * diameter * diameter)) / 6;
-    double growth1 = volume2 - volume1;
-
-    System.out.printf("Increase: %.0f", growth1);
-    System.out.println();
-
-    // Code
-
-    System.out.printf("Increase %.0f", growth2);
-    System.out.println();
-  }
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.print("Diameter: ");
+		double diameter = in.nextDouble();
+		
+		/* code goes here */
+		double volume1 = 3.14 * (diameter * diameter * diameter) / 6;
+		diameter++;
+		double volume2 = 3.14 * (diameter * diameter * diameter) / 6;
+		double growth1 = volume2 - volume1;
+		
+		System.out.printf("Increase: %.0f", growth1);
+		System.out.println();
+		
+		diameter++;
+		double volume3 = 3.14 * (diameter * diameter * diameter) / 6;
+		double growth2 = volume3 - volume2;
+		
+		System.out.printf("Increase: %.0f", growth2);
+		
+		System.out.println();
+	}
 }
